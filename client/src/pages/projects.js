397f4afa52ebec16374c1f764934e414a827c5bf
Maison Moa <maison.moa@gmail.com>
2018-09-21 11:00:20 -0400

    updated details section to be more responsive

--- client/src/pages/projects.js
+++ client/src/pages/projects.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled, { keyframes } from 'styled-components';
 
 const ProjectsPageContainer = styled.div`
@@ -57,6 +57,7 @@ const ProjectCard = styled.div`
     transition: all cubic-bezier(0.075, 0.82, 0.165, 1) .5s;
     display: flex;
     flex-direction: column;
+    position: relative;
 
     &:hover {
         transform: translateY(4px);
@@ -70,13 +71,13 @@ const ProjectImage = styled.div`
     flex-basis: 65%;
 `;
 
-const ProjectDetails = styled.div`
-    flex-basis: 35%;
+const Details = styled.div`
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    align-items: center;
+    height: 100%;
     overflow: auto;
+    justify-content: first baseline;
+    align-items: center;
 
     & > h5 {
         margin-bottom: -2px;
@@ -108,22 +109,26 @@ const ProjectDetails = styled.div`
     }
 `;
 
+const ProjectDetails = styled.div`
+    height: 35%;
+    width: 100%;
+`;
 
 const Projects = () => (
   <ProjectsPageContainer>
     <ProjectsOverviewContainer>
       <OverviewHeading>Projects</OverviewHeading>
       <OverviewText>
-        The project's code can be
-        found via the Github link
-        on the cards. Some projects
-        are hosted and live, and others
-        are living quietly in their repo.
+          The project's code can be
+          found via the Github link
+          on the cards. Some projects
+          are hosted and live, and others
+          are living quietly in their repo.
       </OverviewText>
       <OverviewText>
-        If there is a project you’d like to
-        see live, please contact me and I will get that going for you.
-        Thanks for looking
+          If there is a project you’d like to
+          see live, please contact me and I will get that going for you.
+          Thanks for looking
       </OverviewText>
     </ProjectsOverviewContainer>
     <ProjectCardsContainer>
@@ -131,12 +136,42 @@ const Projects = () => (
         <ProjectCard>
           <ProjectImage />
           <ProjectDetails>
-            <h5> Portfolio Site </h5>
-            <p>
-               This project was built with React, Gatsby, GraphQL, NodeJs and my fingers.
-               Styled-components are used throughout the site.
-            </p>
-            <a href="https://www.github.com"> | Github Repo | </a>
+            <Details>
+              <h5> r/Tronix | Reddit CSS redesign  </h5>
+              <p>
+                 Collaborated with other Reddit members to help write the custom CSS to redesign r/Tronix's CSS layout.
+                 The goal was to create a minimilistic black/white/grey themed layout.
+                 Nothing too fancy here, just pure CSS and a fun project to collaborate on!
+                 Tronix (TRX) is a cryptocurrency, and it's official subreddit has over 67k subscribers currently.
+              </p>
+              <a href="https://www.reddit.com/r/tronix" target="_blank" rel="noopener noreferrer"> | /Tronix | </a>
+            </Details>
+          </ProjectDetails>
+        </ProjectCard>
+        <ProjectCard>
+          <ProjectImage />
+          <ProjectDetails>
+            <Details>
+              <h5> React Weather App Tutorial </h5>
+              <p>
+                    This small weather app was built with the intention of being an example for a tutorial I wrote
+                    on Medium. It was less about the weather and more about connecting a backend Node Express server with a frontend client like React, and sending information back and forth between them. It utalizes a 3rd-party weather api via Open Weather Map to make weather reuests in the backend. The frontend captures a zipcode and then POST's it to the backend that then is used with the weather API qOnce the data is returned, it's fetched by the React component responsible for rendering weather display.
+              </p>
+              <a href="https://www.github.com"> | Github Repo | </a>
+            </Details>
+          </ProjectDetails>
+        </ProjectCard>
+        <ProjectCard>
+          <ProjectImage />
+          <ProjectDetails>
+            <Details>
+              <h5> Portfolio Site </h5>
+              <p>
+                 This project was built with React, Gatsby, GraphQL, NodeJs and my fingers.
+                 Styled-components are used throughout the site.
+              </p>
+              <a href="https://www.github.com"> | Github Repo | </a>
+            </Details>
           </ProjectDetails>
         </ProjectCard>
         <ProjectCard>
@@ -144,30 +179,8 @@ const Projects = () => (
           <ProjectDetails>
             <h5> Portfolio Site </h5>
             <p>
-               This project was built with React, Gatsby, GraphQL, NodeJs and my fingers.
-               Styled-components are used throughout the site.
-            </p>
-            <a href="https://www.github.com"> | Github Repo | </a>
-          </ProjectDetails>
-        </ProjectCard>
-        <ProjectCard>
-          <ProjectImage />
-          <ProjectDetails>
-            <h5> Portfolio Site </h5>
-            <p>
-               This project was built with React, Gatsby, GraphQL, NodeJs and my fingers.
-               Styled-components are used throughout the site.
-            </p>
-            <a href="https://www.github.com"> | Github Repo | </a>
-          </ProjectDetails>
-        </ProjectCard>
-        <ProjectCard>
-          <ProjectImage />
-          <ProjectDetails>
-            <h5> Portfolio Site </h5>
-            <p>
-               This project was built with React, Gatsby, GraphQL, NodeJs and my fingers.
-               Styled-components are used throughout the site.
+                 This project was built with React, Gatsby, GraphQL, NodeJs and my fingers.
+                 Styled-components are used throughout the site.
             </p>
             <a href="https://www.github.com"> | Github Repo | </a>
           </ProjectDetails>
